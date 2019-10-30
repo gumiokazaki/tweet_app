1cca16ac957a362521a2d477694165b8d9b96acf
okazaki <okazaki@gumi.co.jp>
2019-10-30 17:45:26 +0900

    unko

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,29 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  post "likes/:post_id/create" => "likes#create"
+  post "likes/:post_id/destroy" => "likes#destroy"
+  get "users/:id/likes" => "users#likes"
+
+  post "logout" => "users#logout"
+  post "login" => "users#login"
+  get "login" => "users#login_form"
+
+  post 'users/:id/update' => 'users#update'
+
+  get 'users/:id/edit' => 'users#edit'
+  post 'users/create' => 'users#create'
+  get 'signup' => 'users#new'
+  get 'users/index' => 'users#index'
+  get 'users/:id' => 'users#show'
+
+  get 'posts/index' => 'posts#index'
+  get 'posts/new' => 'posts#new'
+  get 'posts/:id' => 'posts#show'
+  post 'posts/create' => 'posts#create'
+  get 'posts/:id/edit' => 'posts#edit'
+  post 'posts/:id/update' => 'posts#update'
+  post 'posts/:id/destroy' => 'posts#destroy'
+
+  get '/' => 'home#top'
+  get 'about' => 'home#about'
 end
